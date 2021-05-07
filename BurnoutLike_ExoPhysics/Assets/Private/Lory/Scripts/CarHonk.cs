8d03b251a193e46cc60d17f29538e717bb18e51d
KyanosAlpha <66301729+KyanosAlpha@users.noreply.github.com>
2021-05-07 16:08:40 +0200

    Honk

---
+++ BurnoutLike_ExoPhysics/Assets/Private/Lory/Scripts/CarHonk.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class CarHonk : MonoBehaviour
+{
+    #region properties
+
+    #endregion properties
+
+
+
+    #region fields
+
+    #endregion fields
+
+
+
+    #region public methods
+
+    #endregion public methods
+
+
+
+    #region unity messages
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+    #endregion unity messages
+
+
+
+    #region private methods
+
+    #endregion private methods
+}
